55bc1b91d69d21e735cc5a8a9df42eb762750357
JP2040 <jeanpaul.gomez@gmail.com>
2017-12-03 12:44:45 -0500

    Fixed mongoDb connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url : 'ec2-18-220-236-162.us-east-2.compute.amazonaws.com:27017/dummyDB'
+    url : 'mongodb://loco39:loco39@ec2-18-220-236-162.us-east-2.compute.amazonaws.com:27017/dummyDB'
 
 };
